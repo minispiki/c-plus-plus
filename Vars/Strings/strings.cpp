7a6608cf0453065f8795ad0b4c879beae6e6b3f3
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-18 12:40:07 +0100

    string function

--- Vars/Strings/strings.cpp
+++ Vars/Strings/strings.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main() {
-    char greetarray[8] = {'W','e','l','c','o','m','e','\0'}; //Array
-    cout << greetarray << "\n";
+    /* char stuff
+    * char greetarray[8] = {'W','e','l','c','o','m','e','\0'}; //Array
+    * cout << greetarray << "\n";
+    *
+    * char greetarraystring[] = "Welcome"; // Array also with string
+    * cout << greetarraystring << "\n";
+    */
 
-    char greetarraystring[] = "Welcome"; // Array also with string
-    cout << greetarraystring << "\n";
+    // ACTUAL strings
+    string greet = "Welcome";
+    cout << greet << "\n";
+
+    // Lets see how long THAT string is
+    cout << "The size of the var 'greet' is " << greet.size() << " characters long. \n";
 }
