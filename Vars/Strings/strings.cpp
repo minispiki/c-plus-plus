02cb3568c08e32563efd23973a77bba8eff9284c
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-18 12:32:17 +0100

    Array with String in strings.cpp

--- Vars/Strings/strings.cpp
+++ Vars/Strings/strings.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    char greet[8] = {'W','e','l','c','o','m','e','\0'}; //Array
-    cout << greet << "\n";
+    char greetarray[8] = {'W','e','l','c','o','m','e','\0'}; //Array
+    cout << greetarray << "\n";
+
+    char greetarraystring[] = "Welcome"; // Array also with string
+    cout << greetarraystring << "\n";
 }
