be3d61a6e10f2bdb2bfe837cccfb6bd5ee9e2775
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-21 09:23:28 +0100

    Get user's age

--- usr interaction/main.cpp
+++ usr interaction/main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
+    int age;
     cout << "Hello, World! \n";
+    cout << "How old are you?";
+    cin >> age; // Only works with one word
+    cout << "So, you are " << age << " years old? \n";
 }
