8245d3ad67d7efd9b857603485cd41a783c86e8e
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-21 09:01:12 +0100

    Start of cpp math

---
+++ math/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float numbers (100); // Supports decimal
+    numbers+=10; /* add 10
+    numbers = numbers + 10; */
+    cout << numbers << "\n";
+}
