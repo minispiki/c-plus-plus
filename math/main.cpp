cca85acc8110e8383f4021ae9384e9037f2d1284
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-21 09:08:04 +0100

    basic math operations

--- math/main.cpp
+++ math/main.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main() {
+    // c++ uses bidmas
     float numbers (100); // Supports decimal
     numbers+=10; /* add 10
     numbers = numbers + 10; */
+    cout << numbers << "\n";
+
+    numbers-=10; /*Subtract 10
+    numbers = numbers - 10; */
+    cout << numbers << "\n";
+
+    numbers*=10; /* multiply by 10
+    numbers = numbers * 10; */
+    cout << numbers << "\n";
+
+    numbers/=10; /* divide by 10
+    numbers = numbers / 10; */
     cout << numbers << "\n";
 }
